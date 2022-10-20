14bf4adec569cd8834e4b27f1b35bc4aa7d45a4c
FRANCO ALBERTO MILLANES ARAYA <Franko20155@gmail.com>
2022-10-20 15:47:01 -0300

    Listo

--- agregarDatos.php
+++ agregarDatos.php
@@ -1,12 +1,25 @@
 <?php
+
 include("conectar.php");
+
 $Comuna=$_GET['txtComuna'];
+
 $consultaAgregar="INSERT INTO comuna(nombre) VALUES ('$Comuna');";
+
 $resultado=mysqli_query($con,$consultaAgregar);
+
 if($resultado){
-echo "Comuna registrada con éxito";
-mysqli_close($con);
+
+?>
+    
+    <script>alert("Comuna registrada con éxito");</script>
+
+<?php
+    header("Location: index.php");
+    mysqli_close($con);
+
 }else{
-echo "No se pudo insertar";
-header("Location: index.php");}
+    echo "No se pudo insertar";
+    header("Location: index.php");}
+
 ?>
